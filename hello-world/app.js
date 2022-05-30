47fe47af94663c0d73aa50dbe4075f86124bd350
hreddy <harishreddy616@gmail.com>
2022-05-31 01:06:11 +0530

    app js text

--- hello-world/app.js
+++ hello-world/app.js
@@ -20,7 +20,7 @@ exports.lambdaHandler = async (event, context) => {
         response = {
             'statusCode': 200,
             'body': JSON.stringify({
-                message: 'hello world harry',
+                message: 'hello world harry reddy',
                 // location: ret.data.trim()
             })
         }
